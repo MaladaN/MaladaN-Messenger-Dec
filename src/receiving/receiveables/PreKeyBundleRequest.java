77fefd4cfd2dd143efe27239a508102d682f1a25
ValliereMagic <ValliereMagic@users.noreply.github.com>
2018-04-18 19:37:09 -0400

    started writing code to handle someone requesting a prekey bundle

--- src/receiving/receiveables/PreKeyBundleRequest.java
+++ src/receiving/receiveables/PreKeyBundleRequest.java
@@ -1,5 +1,13 @@
 package receiving.receiveables;
 
 public class PreKeyBundleRequest implements java.io.Serializable {
+    private String base64RequesterUsername;
 
+    public PreKeyBundleRequest(String base64RequesterUsername) {
+        this.base64RequesterUsername = base64RequesterUsername;
+    }
+
+    public String getBase64RequesterUsername() {
+        return base64RequesterUsername;
+    }
 }
