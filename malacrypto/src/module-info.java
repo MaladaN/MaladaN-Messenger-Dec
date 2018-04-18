61ca834741ab83ed748ba3a6b5cde6549cb779e6
ValliereMagic <ValliereMagic@users.noreply.github.com>
2018-04-18 15:59:40 -0400

    Initial Commit

---
+++ malacrypto/src/module-info.java
@@ -0,0 +1,6 @@
+module malacrypto {
+    requires signal.protocol.java;
+    requires java.sql;
+    requires h2;
+    exports crypto;
+}
---
+++ src/ClientMain.java
@@ -0,0 +1,9 @@
+import crypto.SignalCrypto;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        //initialize signal identity
+        SignalCrypto.initStore();
+    }
+}
